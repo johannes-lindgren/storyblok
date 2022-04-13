6a347160bd3ee90f14b3fc1a83577708eb30758b
johannes-lindgren <johannes.lindgren@storyblok.com>
2022-04-13 11:58:45 +0200

    Removed unused oauth api routes.

--- examples/nextjs-sidebar-app/pages/api/_grant_auth/signin/__storyblok.ts
+++
@@ -1,8 +0,0 @@
-import grant from "grant";
-import {grantConfig} from "@src/grant-config";
-
-export default async function handle(res, req) {
-    const a  = await grant.vercel(grantConfig)(res, req)
-    console.log(a.session)
-}
-
